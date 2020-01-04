f98a241ab279cd1d6808b7b6128dc2337098ebeb
miguelpengson <miguelpengson@gmail.com>
2020-01-04 16:28:23 +0800

    new folders, templates, static with new files base, index and about

--- chikinita.py
+++ chikinita.py
@@ -1,15 +1,29 @@
-
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)    # so flask knows where to find our files
                          # instantiate Flask
+
+posts = [
+    {
+        'author': 'Miguel Pengson',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 2, 2020'
+    },
+    {
+        'author': 'Roberto Pengson',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 2, 2020'
+    }
+]
 @app.route("/")
-@app.route("/home")
-def home():
-    return 'Welcome to Chikinita.com !'
+@app.route("/index")
+def index():
+    return render_template('index.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return 'About Chikinita.com'
+    return render_template('about.html', title='About')
 
 
 
